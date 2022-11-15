267b806ee0b0171b27eea3e5d95f7ad9a61a95af
Ramiro <ramiro174@gmail.com>
2022-11-15 09:16:46 -0600

    ad cambios

--- app/src/main/java/com/gasca1234/yaarto/MainActivity.java
+++ app/src/main/java/com/gasca1234/yaarto/MainActivity.java
@@ -18,6 +18,7 @@ import com.gasca1234.yaarto.Respuesta.Respuesta;
 
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONObject;
 
@@ -40,36 +41,30 @@ public class MainActivity extends AppCompatActivity {
 
         mQueue = SingletonRequest.getInstance(MainActivity.this).getRequestQueue();
         Pokemon = new ArrayList<>();
-        nombre= new ArrayList<>();
+        nombre = new ArrayList<>();
 
         jsparse();
         // Iniciar RecyclerView
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new AdaptadorPokemon(nombre);
-        recyclerView.setAdapter(adapter);
 
     }
 
-    public  void jsparse(){
-        String urlApi ="https://pokeapi.co/api/v2/pokemon";
+    public void jsparse() {
+        String urlApi = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000";
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, urlApi, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                RecyclerView recyclerView = findViewById(R.id.recyclerview);
 
-                    Gson gson = new Gson();
-                    AdaptadorPokemon adapter =new AdaptadorPokemon(nombre);
-                    Respuesta pk = gson.fromJson(String.valueOf(response), (Type) Respuesta.class);
-                    Pokemon.add(new Respuesta(pk.getCount(), pk.getPrevious(), pk.getNext(), pk.getResults()));
-                    List<pokemonNombre> a = pk.getResults();
-                    for (int i = 0; i < a.size(); i++) {
-                        nombre.add(new pokemonNombre(a.get(i).getName(), a.get(i).getUrl()));
-                    }
+                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                Gson gson = new Gson();
+                Respuesta pk = gson.fromJson(response.toString(), Respuesta.class);
+                adapter = new AdaptadorPokemon(pk.getResults());
+                recyclerView.setAdapter(adapter);
 
 
             }
-        },new Response.ErrorListener() {
+        }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
